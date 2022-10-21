c085da827af1a0accf4684e07358711261695981
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-10-21 12:36:50 -0300

    eol in gen format

--- io_dftb_plus.py
+++ io_dftb_plus.py
@@ -84,12 +84,12 @@ def write_gen_format(frame, filename):
         name of the .gen file
     """
     with open(filename, "w") as gen:
-        gen.write(f" {frame.natoms} S")
-        gen.write(" Si Li")
+        gen.write(f" {frame.natoms} S\n")
+        gen.write(" Si Li\n")
         for i, (t, x, y, z) in enumerate(zip(frame.types, frame.x, frame.y, frame.z)):
             atom_type = 1 if t == "Si" else 2
-            gen.write(f"{i + 1} {atom_type} {x:.6e} {y:.6e} {z:.6e}")
-        gen.write("0.0 0.0 0.0")
-        gen.write(f"{frame.box[0]} 0.0 0.0")
-        gen.write(f"0.0 {frame.box[1]} 0.0")
-        gen.write(f"0.0 0.0 {frame.box[2]}")
+            gen.write(f"{i + 1} {atom_type} {x:.6e} {y:.6e} {z:.6e}\n")
+        gen.write("0.0 0.0 0.0\n")
+        gen.write(f"{frame.box[0]} 0.0 0.0\n")
+        gen.write(f"0.0 {frame.box[1]} 0.0\n")
+        gen.write(f"0.0 0.0 {frame.box[2]}\n")
