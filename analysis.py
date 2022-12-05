e3ce5a796574d5330f6f764c4da36a22d6197c9f
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-12-05 14:37:26 -0300

    update analysis

--- analysis.py
+++ analysis.py
@@ -11,14 +11,12 @@ from io_dftb_plus import read_md_out
 
 
 NSI = 64
-
-NORDBLUE = "#5E81AC"
-NORDGREEN = "#A3BE8C"
+NLIMAX = 3.75 * NSI
 
 
-def color_fader(mix):
-    c1 = np.array(matplotlib.colors.to_rgb(NORDBLUE))
-    c2 = np.array(matplotlib.colors.to_rgb(NORDGREEN))
+def color_fader(mix, first_color="#1f77b4", second_color="#2ca02c"):
+    c1 = np.array(matplotlib.colors.to_rgb(first_color))
+    c2 = np.array(matplotlib.colors.to_rgb(second_color))
     return matplotlib.colors.to_hex((1 - mix) * c1 + mix * c2)
 
 
@@ -45,7 +43,7 @@ def rdf_plot(nlis, path="npt/"):
             frames, type_c="Si", type_i="Si", rmax=5.0
         )
         rdf.calculate()
-        rdf.plot(ax=ax, plot_kws={"color": cmap(nli / (3 + max(nlis)))})
+        rdf.plot(ax=ax, plot_kws={"color": cmap(nli / NLIMAX)})
 
     ax.set_xlabel(r"r [$\AA$]")
     ax.set_ylabel("RDF Si-Si")
@@ -80,12 +78,21 @@ def fvc_plot(nlis, path="npt/"):
         err_volume.append(np.std(vx))
 
     df = pd.DataFrame(
-        {"x": x_values, "natoms_a": natoms_a, "volume": volume, "err_volume": volume}
+        {
+            "x": x_values,
+            "natoms_a": natoms_a,
+            "volume": volume,
+            "err_volume": err_volume,
+        }
     )
     df = exma.electrochemistry.fractional_volume_change(df, NSI, 10.937456**3)
 
     fig, ax = plt.subplots()
-    ax.errorbar(df["x"], df["fvc"], yerr=df["err_fvc"])
+
+    ax.errorbar(df["x"], df["fvc"], yerr=df["err_fvc"], marker="o", linestyle=":")
+
+    fvc_chevrier = lambda x: 0.786647 * x + 0.001547
+    ax.plot(df["x"], fvc_chevrier(df["x"]))
 
     ax.set_xlabel(r"$x$ in Li$_x$Si")
     ax.set_ylabel("Fractional volume change")
@@ -98,6 +105,6 @@ def fvc_plot(nlis, path="npt/"):
 
 
 if __name__ == "__main__":
-    nlis = np.arange(1, 26, 3)
+    nlis = np.concatenate((np.arange(1, 22, 4), np.arange(24, 88, 3)))
     rdf_plot(nlis)
     fvc_plot(nlis)
